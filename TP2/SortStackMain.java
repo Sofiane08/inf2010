530487d455b942186ad48c6d76ec6b8646d3f11c
jacob643 <jacobdorais25@hotmail.com>
2018-02-09 17:11:09 -0500

    tp3 commencé, buggé

--- TP2/SortStackMain.java
+++ TP2/SortStackMain.java
@@ -43,12 +43,71 @@ public class SortStackMain
     
 	private static int size(Stack<Integer> stack) {
 		//A completer
-	
+		
+		//enlever éléments en comptant.
+		int qt = 0;
+		Stack<Integer> tempStack = new Stack<Integer>();
+		while(!stack.empty())
+		{
+			qt++;
+			tempStack.push(stack.pop());
+		}
+		
+		//remettre les éléments
+		for(int i = 0; i < qt; i++) { stack.push(tempStack.pop());}
+		
+		return qt;
+		
     }
     
 	static Stack<Integer> sortStack(Stack<Integer> stack)
 	{
 		//A completer
 		
+		//nous allons procéder à un merge sort.
+		//
+		//comme nous ne pouvons utiliser que peek, pop, push et empty, 
+		//nous compterons le nombre d'éléments lors de la séparation.
+		int qt = 0;
+		Stack<Integer> left = new Stack<Integer>();
+		Stack<Integer> right = new Stack<Integer>();
+		
+		while(!stack.empty())
+		{
+			if(qt%2 == 0)
+				left.push(stack.pop());
+			else
+				right.push(stack.pop());
+			qt++;
+		}
+		
+		if(qt == 1) {return left;} //déjà trié; seulement un élément dans left.
+		
+		
+		//sort chaque partie 
+		sortStack(left);
+		sortStack(right);
+		
+		//merge les deux stack:
+		Stack<Integer> inverted = new Stack<Integer>();
+		for(int i = 0; i < qt; i++)
+		{
+			if(left.empty())
+				inverted.push(right.pop());
+			
+			else if(right.empty())
+				inverted.push(left.pop());
+			
+			else if(left.peek() <= right.peek())
+				inverted.push(left.pop());
+			
+			else
+				inverted.push(right.pop());
+		}
+		//inversion de l'ordre pour avoir le plus petit sur le dessus
+		
+		for(int i = 0; i < qt; i++)
+			stack.push(inverted.pop());
+		return stack;
 	}
 }
