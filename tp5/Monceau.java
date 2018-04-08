72b91572d1f542ef6b2ba489ed588c0d172dc0b4
Efix <soisfrancfrancois@hotmail.com>
2018-04-08 07:12:19 -0400

    Commentaires pour la méthode fusion

--- tp5/Monceau.java
+++ tp5/Monceau.java
@@ -14,30 +14,35 @@ public class Monceau {
 		Node arbrethis = null;
 		ArrayList<Node> enordre = new ArrayList<Node>();
 		
-
+		//Pour tout j, en commençant par 0, on vérifie le nombre d'arbres 
+		//d'ordre j dans les 2 monceaux plus la retenue.
 		int j = 0;
 		int maxordre = 0;
 		if(this.arbres.size() != 0) {maxordre = this.arbres.get(this.arbres.size()-1).ordre;}
 		else if(autre.arbres.size() != 0 && autre.arbres.get(autre.arbres.size()-1).ordre > maxordre) 
 			{ maxordre = autre.arbres.get(autre.arbres.size()-1).ordre;}
 		
-		//tant qu<il ny a pu rien
-		//pour tout j
-		for(; j <= maxordre +1; j++)
-		{
-
+		//tant qu'il n'y a plus rien, pour tout j
+		for(; j <= maxordre +1; j++) {
 			int n = 0;
+			
+			//Dans le cas où la retenue est d'ordre j, 
+			//n est incrémenté de 1.
 			if(retenue != null && retenue.ordre == j) n++;
+			
+			//Dans le cas où autre est d'ordre j, 
+			//n est incrémenté de 2.
 			for(int i = 0; i < autre.arbres.size(); i++)
-				if (autre.arbres.get(i).ordre == j)
-				{
+				if (autre.arbres.get(i).ordre == j) {
 					n+=2;
 					arbreautre = autre.arbres.get(i);
 					autre.arbres.remove(i);
 				}
+			
+			//Dans le cas où this est d'ordre j,
+			//n est incrémenté de 4.
 			for(int i = 0; i < this.arbres.size(); i++)
-				if (this.arbres.get(i).ordre == j)
-					{
+				if (this.arbres.get(i).ordre == j) {
 						n+=4;
 						arbrethis = this.arbres.get(i);
 						this.arbres.remove(i);
@@ -45,29 +50,37 @@ public class Monceau {
 			try {
 				switch(n) {
 				case 0:
+					//Il y a aucun arbre d'ordre j
 					break;
 				case 1:
+					//Il y a un seul arbre d'ordre j (retenue)
 					this.arbres.add(retenue);
 					enordre.add(retenue);
 					break;
 				case 2:
+					//Il y a un seul arbre d'ordre j (autre)
 					this.arbres.add(arbreautre);
 					enordre.add(arbreautre);
 					break;
 				case 3:
+					//Il y a deux arbres d'ordre j (retenue et autre)
 					retenue = retenue.fusion(arbreautre);
 					break;
 				case 4:
+					//Il y a un seul arbre d'ordre j (l'arbre this)
 					this.arbres.add(arbrethis);
 					enordre.add(arbrethis);
 					break;
 				case 5:
+					//Il y a deux arbres d'ordre j (retenue et this)
 					retenue = retenue.fusion(arbrethis);
 					break;
 				case 6:
+					//Il y a deux arbres d'ordre j (this et autre)
 					retenue = arbrethis.fusion(arbreautre);
 					break;
 				case 7:
+					//Il y a trois arbres d'ordre j (retenue, this et autre)
 					retenue = retenue.fusion(arbreautre);
 					this.arbres.add(arbrethis);
 					enordre.add(arbrethis);
@@ -75,10 +88,9 @@ public class Monceau {
 
 				default: break;
 				}
-			}
-			catch(DifferentOrderTrees e){}
-			
+			} catch(DifferentOrderTrees e){}
 		}
+		
 		this.arbres = enordre;
 		return;
 	}
